dd1044c2b12449cec9feab9553b313913aab28bf
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-03-03 19:03:17 +0200

    Main menu component added

---
+++ src/BookingSystem/Components/MainMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
--- src/BookingSystem/Controllers/HomeController.cs
+++ src/BookingSystem/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace BookingSystem.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
