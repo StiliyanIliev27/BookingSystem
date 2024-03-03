9eab23f1fc34f6a186f71764bc79c7310fbe42fa
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-03-03 21:45:19 +0200

    Modified the context class

--- src/BookingSystem.Infrastructure/Data/BookingSystemDbContext.cs
+++ src/BookingSystem.Infrastructure/Data/BookingSystemDbContext.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BookingSystem.Infrastructure.Data
+﻿namespace BookingSystem.Infrastructure.Data
 {
+    using BookingSystem.Infrastructure.Data.Models.Location;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Context of the database
+    /// </summary>
     public class BookingSystemDbContext : IdentityDbContext
     {
+        /// <summary>
+        /// Constructor of the context of the database
+        /// </summary>
+        /// <param name="options">Connection options</param>
         public BookingSystemDbContext(DbContextOptions<BookingSystemDbContext> options)
             : base(options)
         {
         }
+
+        /// <summary>
+        /// Continents
+        /// </summary>
+        public DbSet<Continent> Continents { get; set; } = null!;
+
+        /// <summary>
+        /// Countries
+        /// </summary>
+        public DbSet<Country> Countries { get; set; } = null!;
+
+        /// <summary>
+        /// Languages
+        /// </summary>
+        public DbSet<Language> Languages { get; set; } = null!;
+
+        /// <summary>
+        /// CountriesLanguages
+        /// </summary>
+        public DbSet<CountryLanguage> CountriesLanguages { get; set; } = null!;
     }
 }
