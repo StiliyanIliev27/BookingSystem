2412c7e9f15b38950f79200476a5d7b2ef039983
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-03-05 01:37:01 +0200

    Increased details constants max values

--- src/BookingSystem.Infrastructure/Data/Constants/DataConstants.cs
+++ src/BookingSystem.Infrastructure/Data/Constants/DataConstants.cs
@@ -17,7 +17,7 @@
             public const int CapitalMaxLength = 50;
 
             public const int DetailsMinLength = 10;
-            public const int DetailsMaxLength = 500;
+            public const int DetailsMaxLength = 1000;
         }
 
         public static class Language
@@ -34,7 +34,7 @@
             public const int AreaMinLength = 3;
             public const int AreaMaxLength = 50;
 
-            public const int DetailsMaxLength = 200;
+            public const int DetailsMaxLength = 500;
         }
 
         public static class Landmark
@@ -45,7 +45,7 @@
             public const int AddressMinLength = 5;
             public const int AddressMaxLength = 80;
 
-            public const int DetailsMaxLength = 500;
+            public const int DetailsMaxLength = 700;
         }
 
         public static class Hotel
@@ -59,7 +59,7 @@
             public const int StarRateMinValue = 1;
             public const int StarRateMaxValue = 5;
 
-            public const int DetailsMaxLength = 500;
+            public const int DetailsMaxLength = 700;
 
             public const string TimeFormat = "HH:mm";
         }
---
+++ src/BookingSystem.Infrastructure/Migrations/20240304233559_IncreasedDetailsConstantsValues.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingSystem.Infrastructure.Migrations
+{
+    public partial class IncreasedDetailsConstantsValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Landmarks",
+                type: "nvarchar(700)",
+                maxLength: 700,
+                nullable: true,
+                comment: "Landmark's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true,
+                oldComment: "Landmark's Details");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Hotels",
+                type: "nvarchar(700)",
+                maxLength: 700,
+                nullable: true,
+                comment: "Hotel's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true,
+                oldComment: "Hotel's Details");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Countries",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                comment: "Contry's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldComment: "Contry's Details");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Cities",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                comment: "City's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true,
+                oldComment: "City's Details");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Landmarks",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                comment: "Landmark's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(700)",
+                oldMaxLength: 700,
+                oldNullable: true,
+                oldComment: "Landmark's Details");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Hotels",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                comment: "Hotel's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(700)",
+                oldMaxLength: 700,
+                oldNullable: true,
+                oldComment: "Hotel's Details");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Countries",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                comment: "Contry's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldComment: "Contry's Details");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Cities",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                comment: "City's Details",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true,
+                oldComment: "City's Details");
+        }
+    }
+}
