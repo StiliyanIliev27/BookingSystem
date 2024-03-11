f9d6bdf391b78ae4005184d1bf448ece1ea9d37b
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-03-11 19:45:25 +0200

    Added ClaimsPrincipalExtension class with its method to get UserId

---
+++ src/BookingSystem/Extensions/ClaimsPrincipleExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Security.Claims
+{
+    public static class ClaimsPrincipleExtension
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
