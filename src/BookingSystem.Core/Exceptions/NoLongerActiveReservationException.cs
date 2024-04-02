d178104cd4fa378f356a4be9fbbefded5b3858f4
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-04-02 21:14:39 +0300

    Created two custom Error Handling Exceptions for Unauthorized actions and No Longer Available Reservations actions

---
+++ src/BookingSystem.Core/Exceptions/NoLongerActiveReservationException.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingSystem.Core.Exceptions
+{
+    public class NoLongerActiveReservationException : Exception
+    {
+        public const string NoLongerActiveReservationErrorMessage = "The reservation is no longer available!";
+        public NoLongerActiveReservationException() 
+            : base(NoLongerActiveReservationErrorMessage)
+        {
+            
+        }
+    }
+}
---
+++ src/BookingSystem.Core/Exceptions/UnauthorizedActionException.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingSystem.Core.Exceptions
+{
+    public class UnauthorizedActionException : Exception
+    {
+        public const string UnauthorizedErrorMessage = "The current user is not the owner of the reservation!";
+        public UnauthorizedActionException() 
+            : base(UnauthorizedErrorMessage)
+        {
+            
+        }
+    }
+}
