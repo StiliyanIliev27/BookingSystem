562cdd7de261385b9c778f401fbffcecc9d5611d
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-04-15 14:17:36 +0300

    Added BookingSystem.Tests project for NUnit testing

---
+++ src/BookingSystem.Tests/UnitTests/HotelServiceTests.cs
@@ -0,0 +1,17 @@
+namespace BookingSystem.Tests.UnitTests
+{
+    [TestFixture]
+    public class HotelServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
